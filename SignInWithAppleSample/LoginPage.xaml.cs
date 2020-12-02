244ca503e3b8ac289690c2e8cfb0e1c6a8b18113
Alexander Kotik <alexander.kotik@noris-group.com>
2020-12-02 08:00:18 +0100

    first commit

---
+++ SignInWithAppleSample/LoginPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using SignInWithAppleSample.ViewModels;
+using Xamarin.Forms;
+
+namespace SignInWithAppleSample
+{
+    public partial class LoginPage : ContentPage
+    {
+        LoginViewModel _vm;
+        public LoginPage()
+        {
+            _vm = new LoginViewModel();
+            _vm.OnSignIn += OnSignIn;
+            BindingContext = _vm;
+            InitializeComponent();
+        }
+
+        private void OnSignIn(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+
+        async void  AppleSignInButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await _vm.OnAppleSignInRequest();
+        }
+    }
+}
